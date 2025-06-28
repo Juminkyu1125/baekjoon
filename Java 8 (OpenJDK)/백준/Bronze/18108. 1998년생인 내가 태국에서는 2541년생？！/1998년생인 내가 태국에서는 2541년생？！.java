a94cb4758de379f0a9d7d6f96c1c52006f11f4cf
Juminkyu1125 <zzmg11251125@gmail.com>
2025-06-28 16:09:33 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 176 ms, Memory: 17676 KB -BaekjoonHub

---
+++ Java 8 (OpenJDK)/백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int year1 = scan.nextInt();
+        int year2 = year1-543;
+        System.out.print(year2);
+    }
+}
