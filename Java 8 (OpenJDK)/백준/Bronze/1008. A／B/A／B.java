57d9550de145077901529194482074655f97d61b
Juminkyu1125 <zzmg11251125@gmail.com>
2025-06-28 15:51:34 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 17764 KB -BaekjoonHub

---
+++ Java 8 (OpenJDK)/백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        double A = scan.nextInt();
+        double B = scan.nextInt();
+        double sum = A/B;
+        System.out.println(sum);
+    }
+}
