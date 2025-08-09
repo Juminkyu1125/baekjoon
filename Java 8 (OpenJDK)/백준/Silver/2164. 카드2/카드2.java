0c9d1ece3fcd73a9e804d49d39e21d8398284d0b
Juminkyu1125 <zzmg11251125@gmail.com>
2025-08-09 14:38:51 +0900

    [Silver IV] Title: 카드2, Time: 232 ms, Memory: 49000 KB -BaekjoonHub

---
+++ Java 8 (OpenJDK)/백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        Queue<Integer> q = new LinkedList<>();
+        for (int i =1; i<=N; i++){
+            q.add(i);
+        }
+        while (q.size()>1){
+            q.poll();
+            q.add(q.poll());
+        }
+        System.out.println(q.peek());
+    }
+}
